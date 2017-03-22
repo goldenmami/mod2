3215818cb69ee05a6c38362206115f978dcca8e5
goldenmami <mami.a7med@gmail.com>
2017-03-22 02:28:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
-include "connexion.php";
+include "file.json";
 echo "Hello world from Nokia!";
-$req = $bdd->prepare('INSERT INTO commande(date, civilite, nom, prenom, societe, email, tel, service, pack, type, opt_1, opt_2, opt_3, opt_4, opt_5, precision)
-VALUES(:date, :civilite, :nom, :prenom, :societe, :email, :tel, :service, :pack, :type, :opt_1, :opt_2, :opt_3, :opt_4, :opt_5, :precision)')
-or exit(print_r($bdd->errorInfo()));
-                                   
-$req->execute(array(
-'date' => $date,
-'civilite' => $civilite,
-'nom' => $nom,
-'prenom' => $prenom,
-'societe' => $societe,
-'email' => $email,
-'tel' => $tel,
-'service' => $service,
-'pack' => $pack,
-'type' => $type,
-'opt_1' => $opt_1,
-'opt_2' => $opt_2,
-'opt_3' => $opt_3,
-'opt_4' => $opt_4,
-'opt_5' => $opt_5,
-'precision' => $precision
-));
+?>
