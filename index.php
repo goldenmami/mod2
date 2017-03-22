286f18fdec11ace59045d8bdcf806abc49f36080
goldenmami <mami.a7med@gmail.com>
2017-03-22 08:53:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-include "file.json";
-echo "Hello world from Nokia!";
+echo "***************************************";
+echo "*                                     *";
+echo "*         hello Nokia!                *";
+echo "*                                     *";
+echo "***************************************";
+//$str = file_get_contents('file.json');
+$str = file_get_contents('http://host-php.paas.msv-project.com/file.json');
+$json = json_decode($str, true);
+echo '<pre>' . print_r($json, true) . '</pre>';
+$temperatureMin = $json['daily']['data'][0]['temperatureMin'];
+$temperatureMax = $json['daily']['data'][0]['temperatureMax'];
 ?>
